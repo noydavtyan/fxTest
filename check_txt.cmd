ff1a9ba795755ad5b360595135709c1e4e3c5e98
ndavtyan <6@localhost>
2021-03-02 12:42:07 +0100

    delete all in cmd

--- check_txt.cmd
+++ check_txt.cmd
@@ -1,19 +1,3 @@
 
-Rem Turns the echo off so that each command will not be shown when executed 
-@echo off 
-echo "Hello World" 
-
-if not exist *.txt (
-echo This directory contains no text files.
-
-) else (
-   Rem Turns the echo on so that each command will be shown as executed 
-   echo on 
-   echo "Hello World"
-   echo This directory contains the following text files:
-   echo.
-   dir /b *.txt
-
-   )
  
 
